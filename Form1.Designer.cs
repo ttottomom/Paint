2e4b0d949fa6d415c111c90d1cf64979268cc2f2
ttottomom <37243563+ttottomom@users.noreply.github.com>
2020-04-05 20:32:21 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart_Click = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(290, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // btnStart_Click
+            // 
+            this.btnStart_Click.Location = new System.Drawing.Point(53, 263);
+            this.btnStart_Click.Name = "btnStart_Click";
+            this.btnStart_Click.Size = new System.Drawing.Size(139, 69);
+            this.btnStart_Click.TabIndex = 3;
+            this.btnStart_Click.Text = "Thread 시작";
+            this.btnStart_Click.UseVisualStyleBackColor = true;
+            this.btnStart_Click.Click += new System.EventHandler(this.btnStart_Click_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(240, 263);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(140, 69);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Thread 중지";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart_Click);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart_Click;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        private Thread m_threadPlus;
+        private Thread m_threadMinus;
+
+        private int m_nValue = 0;
+        private int m_nPlusCount = 0;
+        private int m_nMinusCount = 0;
+
+        public delegate void DelegateShowText(string strLabel, string strText);
+        public DelegateShowText DelegateShowTextInstance;
+        
+        public Form1()
+        {
+            DelegateShowTextInstance = new DelegateShowText(this.ShowText);
+            InitializeComponent();
+        }
+
+        private void ShowText(string strLabel, string StrText)
+        {
+            switch(strLabel)
+            {
+                case "label1":
+                    label1.Text = StrText;
+                    break;
+                case "label2":
+                    label2.Text = StrText;
+                    break;
+                case "label3":
+                    label3.Text = StrText;
+                    break;
+            }
+        }
+
+        private void btnStart_Click_Click(object sender, EventArgs e)
+        {
+            m_threadPlus = new Thread(new ThreadStart(ThreadPlus));
+            m_threadMinus = new Thread(new ThreadStart(ThreadMinus));
+
+            m_threadPlus.Start();
+            m_threadMinus.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (m_threadPlus.IsAlive == true)
+                m_threadPlus.Abort();
+            if (m_threadMinus.IsAlive == true)
+                m_threadMinus.Abort();
+        }
+        
+        private void ThreadPlus()
+        {
+            lock (this)
+            {
+                while (m_nPlusCount < 1000)
+                {
+                    int nValue = m_nValue + 1;
+                    Thread.Sleep(1);
+                    m_nValue = nValue;
+                    Invoke(DelegateShowTextInstance, new object[] { "label1", Convert.ToString(m_nValue) });
+                    m_nPlusCount++;
+                    Invoke(DelegateShowTextInstance, new object[] { "label2", Convert.ToString(m_nPlusCount) });
+                }
+            }
+        }
+
+        private void ThreadMinus()
+        {
+            lock (this)
+            {
+                while (m_nMinusCount > -1000)
+                {
+                    int nValue = m_nValue - 1;
+                    Thread.Sleep(1);
+                    m_nValue = nValue;
+                    //label1.Text = Convert.ToString(m_nValue);
+                    Invoke(DelegateShowTextInstance, new object[] { "label1", Convert.ToString(m_nValue) });
+                    m_nMinusCount--;
+                    //label3.Text = Convert.ToString(m_nMinusCount);
+                    Invoke(DelegateShowTextInstance, new object[] { "label3", Convert.ToString(m_nMinusCount) });
+                }
+            }
+        }
+    }
+}
